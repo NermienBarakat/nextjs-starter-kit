235f51238c459d714552bfc6fb6424f362dd6d0b
NermienBarakat <nermienbarakat@gmail.com>
2024-11-27 19:10:55 +0000

    Install Tailwind CSS and PostCSS

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,6 +3,9 @@ export default function HomePage() {
     <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
       <main className="flex flex-col gap-8 row-start-2 items-center sm:items-start">
         <h1>Welcome to next starter kit</h1>
+        <button className="shadow-md inset-shadow-sm inset-shadow-white/20 ring ring-blue-600 inset-ring inset-ring-white/15 bg-amber-400 rounded-2xl p-2">
+          Test Button
+        </button>
       </main>
     </div>
   );
